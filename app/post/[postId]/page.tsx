8cd0ad71ab0c1fc6693a6c3dee087b3d07ce1b94
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2025-03-16 12:21:24 +0900

    [style] post 에디터 부분만 스크롤 되도록 수정

--- app/post/[postId]/page.tsx
+++ app/post/[postId]/page.tsx
@@ -1,22 +1,15 @@
-"use client";
 import { cn } from "@/lib/utils";
 import { Database } from "@/types/supabase";
 import { Separator } from "@radix-ui/react-separator";
 import { PostgrestSingleResponse } from "@supabase/supabase-js";
-import {
-  Breadcrumb,
-  BreadcrumbList,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbSeparator,
-  BreadcrumbPage,
-} from "@ui/breadcrumb";
-import { Button } from "@ui/button";
-import { PanelLeftIcon } from "lucide-react";
-import { useEffect, useRef } from "react";
+
 import "@mdxeditor/editor/style.css";
 import MarkdownEditor from "@/components/markdown/markdown-editor";
 import { postDummyDataString } from "@/app/post/[postId]/dummy-data";
+import { EditableDiv } from "@ui/editable-div";
+import { SidebarTrigger } from "@ui/sidebar";
+import PostBreadcrumb from "@/components/post/main/post-breadcrumb";
+import PostControllerWrapper from "@/components/post/main/post-controller/post-controller-wrapper";
 // import dynamic from "next/dynamic";
 
 export default function Page() {
@@ -28,10 +21,11 @@ export default function Page() {
   return (
     <div className="bg-gray-200 w-full h-full">
       <main className="p-2">
-        <MainContainer className="bg-white">
+        {/* 상단바의 크기를 4rem이라고 가정 */}
+        <MainContainer className="bg-white md:max-h-[calc(100vh-4rem)]">
           <header
             data-component-name="main-header"
-            className="flex h-16 shrink-0 items-center gap-2"
+            className="flex h-16 shrink-0 items-center gap-2 justify-between"
           >
             <div className="flex items-center gap-2 px-4">
               <SidebarTrigger className="-ml-1" />
@@ -39,26 +33,15 @@ export default function Page() {
                 orientation="vertical"
                 className="mr-2 data-[orientation=vertical]:h-4"
               />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="#">카테고리명</BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="#">서브카테고리</BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>제목</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
+              <PostBreadcrumb />
+            </div>
+            <div className="flex items-center gap-2 px-4">
+              <PostControllerWrapper />
             </div>
           </header>
           <section
             data-component-name="main-post-section"
-            className="flex flex-1 flex-col gap-4 p-4 md:px-8 pt-0"
+            className="flex flex-1 flex-col gap-4 p-4 md:px-8 pt-0 overflow-auto scrollbar "
           >
             <h2
               data-component-name="main-post-title"
@@ -75,43 +58,6 @@ export default function Page() {
   );
 }
 
-/**
- * 사용자가 직접 편집할 수 있는 div 요소
- * @param {string} defaultValue - 초기 텍스트 값
- * @param {(value: string) => void} onInput - 입력값이 변경될 때 호출되는 콜백 함수
- * @param {boolean} isEditable - 수정 가능 여부
- */
-export const EditableDiv: React.FC<{
-  defaultValue?: string;
-  onInput?: (value: string) => void;
-  isEditable?: boolean;
-}> = ({ defaultValue = "", onInput, isEditable = true }) => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  /** 입력 이벤트 핸들러 */
-  const handleInput = () => {
-    const newValue = ref.current?.innerText || "";
-    onInput?.(newValue);
-  };
-
-  /** defaultValue 변경 시 div 내용 업데이트 */
-  useEffect(() => {
-    if (ref.current) {
-      ref.current.innerText = defaultValue;
-    }
-  }, [defaultValue]);
-
-  return (
-    <div
-      ref={ref}
-      contentEditable={isEditable}
-      suppressContentEditableWarning
-      className="h-full w-full break-words whitespace-pre-wrap focus:outline-offset-8 focus:outline-dotted"
-      onInput={handleInput}
-    />
-  );
-};
-
 function MainContainer({ className, ...props }: React.ComponentProps<"main">) {
   return (
     <main
@@ -123,28 +69,5 @@ function MainContainer({ className, ...props }: React.ComponentProps<"main">) {
       )}
       {...props}
     />
-  );
-}
-
-function SidebarTrigger({
-  className,
-  onClick,
-  ...props
-}: React.ComponentProps<typeof Button>) {
-  return (
-    <Button
-      data-sidebar="trigger"
-      data-slot="sidebar-trigger"
-      variant="ghost"
-      size="icon"
-      className={cn("size-7", className)}
-      onClick={(event) => {
-        onClick?.(event);
-      }}
-      {...props}
-    >
-      <PanelLeftIcon />
-      <span className="sr-only">Toggle Sidebar</span>
-    </Button>
   );
 }
