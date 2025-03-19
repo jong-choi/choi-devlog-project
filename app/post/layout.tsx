2964af665bf92582c152936ca6ea9a902db1e532
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2025-03-19 16:26:54 +0900

    [chore] post 레이아웃 위치 변경

--- app/post/layout.tsx
+++ app/post/layout.tsx
@@ -1,24 +1,23 @@
-import PostSidebar from "@/components/post/sidebar/post-sidebar";
 import PostTopBar from "@/components/post/topBar/post-top-bar";
 
-interface TodoLayoutProps {
+interface PostLayoutProps {
   params?: Promise<{ postId: string }>;
   children: React.ReactNode;
 }
 
-export default async function TodoLayout({
-  params,
+export default async function PostLayout({
+  // params,
   children,
-}: TodoLayoutProps) {
-  const postId = (await params)?.postId || "";
+}: PostLayoutProps) {
+  // const postId = (await params)?.postId || "";
   return (
     <main className="w-full h-full flex flex-col">
       <PostTopBar />
       <section className="flex flex-row flex-1">
-        <div className="w-64 h-full border-r">
+        {/* <div className="w-64 h-full border-r">
           <PostSidebar postId={postId} />
-        </div>
-        <article className="flex-1 overflow-auto">{children}</article>{" "}
+        </div> */}
+        <article className="flex-1 overflow-auto">{children}</article>
       </section>
     </main>
   );
