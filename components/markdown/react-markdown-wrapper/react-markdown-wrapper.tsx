5cfe0332a45740a7b0328ca7e49d48dd683d8405
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2025-03-19 14:37:43 +0900

    [feat] pin 번호로 로그인 추가

--- components/markdown/react-markdown-wrapper/react-markdown-wrapper.tsx
+++ components/markdown/react-markdown-wrapper/react-markdown-wrapper.tsx
@@ -6,8 +6,7 @@ import rehypeHighlight from "rehype-highlight";
 import "highlight.js/styles/github.css"; // rehype-highlight 스타일 추가
 import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
-import { createClient } from "@/utils/supabase/client";
-import { User } from "@supabase/supabase-js";
+import { useAuthStore } from "@/hooks/use-auth-store";
 
 interface ReactMarkdownWrapperProps {
   children?: string;
@@ -18,28 +17,7 @@ export default function ReactMarkdownWrapper({
 }: ReactMarkdownWrapperProps) {
   const [isMounted, setIsMounted] = useState(false);
   const [isMessage, setIsMessage] = useState(false);
-  const [user, setUser] = useState<User | null>(null);
-  const supabase = createClient();
-
-  useEffect(() => {
-    // ✅ 현재 로그인된 세션 가져오기
-    async function getSession() {
-      const { data } = await supabase.auth.getSession();
-      setUser(data.session?.user || null);
-    }
-    getSession();
-
-    // ✅ 로그인 상태 변경 감지
-    const { data: authListener } = supabase.auth.onAuthStateChange(
-      (_event, session) => {
-        setUser(session?.user || null);
-      }
-    );
-
-    return () => {
-      authListener.subscription.unsubscribe();
-    };
-  }, [supabase]);
+  const isValid = useAuthStore((state) => state.isValid);
 
   useEffect(() => {
     if (!isMounted && !isMessage) {
@@ -59,7 +37,7 @@ export default function ReactMarkdownWrapper({
       >
         <div>
           게시글을 클릭하여 수정
-          {user ? (
+          {isValid ? (
             ""
           ) : (
             <span className="text-rose-600 text-xs">(게스트 모드)</span>
