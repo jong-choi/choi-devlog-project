031021c3381f0e1654a179e913facdd567e94791
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2025-03-30 20:32:46 +0900

    [feat] 캐시래퍼 함수 새거 추가

--- utils/nextCache.tsx
+++ utils/nextCache.tsx
@@ -1,4 +1,10 @@
+import { Database } from "@/types/supabase";
 import { createClient } from "@/utils/supabase/server";
+import {
+  PostgrestResponse,
+  PostgrestSingleResponse,
+  SupabaseClient,
+} from "@supabase/supabase-js";
 import { unstable_cache } from "next/cache";
 import { cookies } from "next/headers";
 
@@ -14,6 +20,7 @@ export const CACHE_TAGS = {
   },
   POST: {
     ALL: () => "posts",
+    BY_PAGE: (page: number) => "posts:by_page:" + page.toLocaleString(),
     BY_URL_SLUG: (urlSlug: string = "") => "post:by_url_slug:" + urlSlug,
     BY_SUBCATEGORY_ID: (subcategoryId: string = "") =>
       "posts:by_subcategory_id:" + subcategoryId,
@@ -63,4 +70,66 @@ export const createWithInvalidation = <T, A extends unknown[]>(
     invalidateFn(result);
     return result;
   };
+};
+
+// 새로운 캐시함수 래퍼
+interface WithCacheParams<P, T, Single extends boolean = false> {
+  key: string[];
+  tags: string[];
+  revalidate?: number;
+  skipCache?: (params: P) => boolean;
+  single?: Single;
+  handler: (
+    supabase: SupabaseClient<Database>,
+    params: P
+  ) => Promise<WithSupabaseReturn<T, Single>>;
+}
+
+// 응답 타입 분기 유틸
+type WithSupabaseReturn<T, Single extends boolean> = Single extends true
+  ? PostgrestSingleResponse<T>
+  : PostgrestResponse<T>;
+
+/**
+ * Supabase Client를 기반으로 캐싱된 서버 요청을 실행합니다.
+ *
+ * - 캐시 키 및 태그를 통해 `unstable_cache`를 구성합니다.
+ * - `single` 옵션을 통해 단건(single) 또는 다건(list) 응답을 선택할 수 있습니다.
+ * - `skipCache` 조건이 true일 경우 캐시를 우회하고 항상 fresh 요청을 실행합니다.
+ *
+ * @template P - 파라미터 타입
+ * @template T - 반환될 데이터의 row 타입
+ * @template Single - 단건 여부 (`true`이면 PostgrestSingleResponse, 아니면 PostgrestResponse)
+ *
+ * @param params - Supabase 요청에 전달할 파라미터
+ * @param options - 캐싱과 핸들러 설정
+ * @param options.handler - Supabase 요청을 수행할 비동기 핸들러
+ * @param options.key - unstable_cache에 사용할 고유한 캐시 키
+ * @param options.tags - 캐시 무효화에 사용할 태그 목록
+ * @param options.revalidate - 캐시 재검증 주기 (초 단위, 기본값: 60초)
+ * @param options.skipCache - 조건부 캐시 우회 함수 (true일 경우 handler를 직접 호출)
+ * @param options.single - 단건 조회 여부 (true 시 PostgrestSingleResponse<T> 반환)
+ *
+ * @returns Supabase 응답 객체 (단건 또는 다건 응답)
+ */
+export const withSupabaseCache = async <P, T, Single extends boolean = false>(
+  params: P,
+  options: WithCacheParams<P, T, Single>
+): Promise<WithSupabaseReturn<T, Single>> => {
+  const supabase = await createClient(await cookies());
+
+  if (options.skipCache?.(params)) {
+    return options.handler(supabase, params);
+  }
+
+  const cached = unstable_cache(
+    () => options.handler(supabase, params),
+    options.key,
+    {
+      revalidate: options.revalidate ?? 60,
+      tags: options.tags,
+    }
+  );
+
+  return cached();
 };
