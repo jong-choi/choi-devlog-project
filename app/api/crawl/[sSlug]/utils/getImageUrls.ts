f0db8037b6a55ffe4f8b20e4acd6b631f1945c42
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2025-04-03 19:59:36 +0900

    [fix] 이미지 업로드 - png 말고 다른 이미지도 업로드

--- app/api/crawl/[sSlug]/utils/getImageUrls.ts
+++ app/api/crawl/[sSlug]/utils/getImageUrls.ts
@@ -11,9 +11,10 @@ export default function getImageUrls(seriesData: VelogAPIResponse): string[] {
     const body = seriesPost.post.body;
 
     if (body) {
-      // 정규 표현식을 사용하여 이미지 URL 추출 (post/.../.../png 형태로 변경)
+      // // 정규 표현식을 사용하여 이미지 URL 추출 (post/.../.../png 형태로 변경)
+      // png 말고 다른 확장자도 추가하도록 수정.
       const regex =
-        /\(https:\/\/velog\.velcdn\.com\/images\/bluecoolgod80\/(post\/[a-zA-Z0-9-]+\/[a-zA-Z0-9-]+\.png)\)/g;
+        /\(https:\/\/velog\.velcdn\.com\/images\/bluecoolgod80\/(post\/[a-zA-Z0-9-]+\/[a-zA-Z0-9-]+\.(png|jpg|jpeg|gif|webp))\)/g;
       let match;
 
       while ((match = regex.exec(body)) !== null) {
