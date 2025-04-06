91af16313f4ae6fc36fc67acd5b4f14c0e98abe2
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2025-04-07 04:47:24 +0900

    [fix] next image 태그에서 src가 falsy일 때 분기처리

--- components/markdown/react-markdown-app.tsx
+++ components/markdown/react-markdown-app.tsx
@@ -12,17 +12,20 @@ export default function ReactMarkdownApp({ children }: { children?: string }) {
       remarkPlugins={[remarkGfm, remarkParse, remarkBreaks]}
       rehypePlugins={[rehypeHighlight]}
       components={{
-        img: ({ src = "", alt = "" }) => (
-          <Image
-            src={src}
-            alt={alt}
-            // https://stackoverflow.com/questions/69230343/nextjs-image-component-with-fixed-witdth-and-auto-height
-            width="0" // width와 height에 0을 줘서 렌더링 계산에서 제외
-            height="0"
-            sizes="(max-width: 768px) 100vw, 50vw" // 이미지 크기 힌팅 : sm사이즈에서 100vw, 그 이상에서 50vw에 맞는 이미지로 불러오기 (supabase가 이미지 프록시 서버가 아니라서 작동 안하긴 함)
-            className="w-auto h-[180px] sm:h-[220px] md:h-[260px] lg:h-[300px] xl:h-[340px]" // css를 통해 실제 렌더링 크기를 확정
-          />
-        ),
+        img: ({ src, alt = "" }) => {
+          if (!src) return null;
+          return (
+            <Image
+              src={src}
+              alt={alt}
+              // https://stackoverflow.com/questions/69230343/nextjs-image-component-with-fixed-witdth-and-auto-height
+              width="0" // width와 height에 0을 줘서 렌더링 계산에서 제외
+              height="0"
+              sizes="(max-width: 768px) 100vw, 50vw" // 이미지 크기 힌팅 : sm사이즈에서 100vw, 그 이상에서 50vw에 맞는 이미지로 불러오기 (supabase가 이미지 프록시 서버가 아니라서 작동 안하긴 함)
+              className="w-auto h-[180px] sm:h-[220px] md:h-[260px] lg:h-[300px] xl:h-[340px]" // css를 통해 실제 렌더링 크기를 확정
+            />
+          );
+        },
       }}
     >
       {children}
