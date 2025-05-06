2a6a2acc078093f15c6b3ab0524b8cc487dc8921
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2025-05-07 03:06:49 +0900

    [fix] series 타입 수정

--- types/series.ts
+++ types/series.ts
@@ -1,4 +1,4 @@
 import { Database } from "@/types/supabase";
 
 export type Series =
-  Database["public"]["Views"]["subcategories_with_meta"]["Row"];
+  Database["public"]["Views"]["subcategories_with_published_meta"]["Row"];
