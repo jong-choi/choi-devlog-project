331a2a3ed6b67f202d1fce3a95784ce7cbe01c33
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2025-05-08 22:08:05 +0900

    [feat] Post 페이지에서 URL SLUG를 encode해서 사용

--- app/post/[urlSlug]/page.tsx
+++ app/post/[urlSlug]/page.tsx
@@ -1,8 +1,9 @@
-export const dynamic = "force-static";
+export const revalidate = 60 * 60 * 24 * 30;
 
 import { getPostByUrlSlug } from "@/app/post/fetchers";
 import PostPageRenderer from "@/components/post/page/page-renderer";
 import RedirectTo from "@ui/redirect-to";
+import { redirect } from "next/navigation";
 
 interface PageProps {
   params: Promise<{
@@ -12,13 +13,18 @@ interface PageProps {
 
 export default async function Page({ params }: PageProps) {
   const { urlSlug } = await params;
+  const encodedSlug = encodeURIComponent(decodeURIComponent(urlSlug));
+  if (urlSlug !== encodedSlug) {
+    redirect(`/post/${encodedSlug}`);
+  }
+
   const result = await getPostByUrlSlug({
     urlSlug: decodeURIComponent(urlSlug),
   });
   const { data } = result;
 
   if (!data || data.is_private) {
-    return <RedirectTo to={`/post/${urlSlug}/private`} />;
+    return <RedirectTo to={`/post/${encodedSlug}/private`} />;
   }
 
   return <PostPageRenderer data={data} />;
