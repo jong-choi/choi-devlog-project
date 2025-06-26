ae5ff2e722892087248063305cacb1a71fdfaed3
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2025-06-26 09:51:35 +0900

    [refactor] JSDOM을 백엔드에서 렌더링한 후 캐싱하도록

--- components/cluster/graph/cluster-graph-svg.tsx
+++ components/cluster/graph/cluster-graph-svg.tsx
@@ -1,12 +1,12 @@
-// import { getClusterJSDOM } from "@/app/map/fetchers";
+import { getClusterJSDOM } from "@/app/map/fetchers";
 
 export default async function ClusterGraphSVG() {
-  // const { html } = await getClusterJSDOM();
+  const { html } = await getClusterJSDOM();
   return (
     <div
       className="w-full h-full max-h-full min-h-0
       overflow-hidden cursor-grabbing"
-      // dangerouslySetInnerHTML={{ __html: html }}
+      dangerouslySetInnerHTML={{ __html: html }}
     />
   );
 }
